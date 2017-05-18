43045868bc2999101b4239f26708fa01560d178e
Stuart Thomson <swdt1@students.waikato.ac.nz>
2017-05-19 11:48:21 +1200

    Better paragraph parsing
    Modified text to remove the line-length restriction. Paragraphs with newlines for formatting still have newlines in them.

--- src/components/Paragraph.js
+++ src/components/Paragraph.js
@@ -4,9 +4,21 @@ import './Paragraph.css';
 class Paragraph extends Component {
   render() {
     // TODO: Do name substitution?
+    let lines = this.props.text.split(/\r?\n/);
+    let elements = [];
+
+    for (let i = 0; i < lines.length; i++) {
+      let line = lines[i];
+      
+      elements.push(`${line} `);
+      if (i < (lines.length - 1)) {
+        elements.push(<br />);
+      }
+    }
+
     return (
       <p className="Paragraph">
-        {this.props.text}
+        {elements}
       </p>
     );
   }
