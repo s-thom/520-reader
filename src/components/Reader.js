74c9f0f90684097054f8a976b39093bbe3665bbd
Stuart Thomson <swdt1@students.waikato.ac.nz>
2017-05-19 15:06:02 +1200

    Add drag gesture to change pages

--- src/components/Reader.js
+++ src/components/Reader.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import Page from './Page';
-import {paginate} from '../util';
+import {paginate, dimensions} from '../util';
+import './Reader.css';
 
 class Reader extends Component {
   constructor(props) {
@@ -14,34 +15,79 @@ class Reader extends Component {
 
     this.pages = paginate(this.props.text)
       .map((t, i) => <Page text={t} identifier={i} key={i} />);
+
+    this.startPosition = null;
+    this.currPosition = null;
+    this.pageContainer = null;
   }
 
   render() {
     return (
       <div className="Reader">
-        <div className="page-container">
+        <div 
+          className="page-container"
+          ref={(c) => this.pageContainer = c}
+          onTouchStart={(e) => this.mouseDown(e)}
+          onTouchEnd={(e) => this.mouseUp(e)}
+          onTouchMove={(e) => this.mouseMove(e)}
+          >
           {this.pages[this.state.page]}
         </div>
         <div className="temp-nav">
-          <button onClick={()=> this.prevPage()}>Prev</button>
-          <button onClick={()=> this.nextPage()}>Next</button>
+          <button onClick={() => this.prevPage()}>Prev</button>
+          <button onClick={() => this.nextPage()}>Next</button>
         </div>
       </div>
     );
   }
 
   nextPage() {
-    this.setPage(this.state.page + 1);
+    this.setPage(Math.min(this.state.page + 1, this.pages.length));
   }
 
   prevPage() {
-    this.setPage(this.state.page - 1);
+    this.setPage(Math.max(this.state.page - 1, 0));
   }
 
   setPage(page) {
     this.setState({
       page
     });
+  }
+
+  mouseDown(event) {
+    if (!this.currPosition) {
+      this.startPosition = event.touches[0];
+      this.currPosition = this.startPosition;
+    }
+  }
+
+  mouseUp(event) {
+    if (event.touches.length === 0) {
+      let xDiff = this.startPosition.clientX - this.currPosition.clientX;
+      if (xDiff > 100) {
+        this.nextPage();
+      } else if (xDiff < -100) {
+        this.prevPage();
+      }
+
+      this.updateTurning(0);
+
+      this.currPosition = null;
+      this.startPosition = null;
+    }
+  }
+
+  mouseMove(event) {
+    this.currPosition = event.touches[0];
+
+
+    let xDiff = this.currPosition.clientX - this.startPosition.clientX;
+    this.updateTurning(xDiff);
+  }
+
+  updateTurning(diff) {
+    this.pageContainer.style.left = `${diff}px`;
   }
 }
 
