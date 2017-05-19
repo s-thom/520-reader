05335e486fcaaf8ecab280fdbec214adfe9b9508
Stuart Thomson <swdt1@students.waikato.ac.nz>
2017-05-19 15:55:42 +1200

    Create BookLine files

---
+++ src/components/BookLine.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import './BookLine.css';
+
+/**
+ * Component to display the occurence of a name throughout the text
+ * 
+ * @class BookLine
+ * @extends {Component}
+ */
+class BookLine extends Component {
+  render() {
+    return (
+      <div className="BookLine">
+        {/* TODO: Render bookline */}
+      </div>
+    );
+  }
+}
+
+export default BookLine;
