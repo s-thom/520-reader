805a488c217577d19bec69b46b4ca70baf1a93e1
Stuart Thomson <swdt1@students.waikato.ac.nz>
2017-05-28 10:33:45 +1200

    Allow selection of line type

--- src/components/BookLine.js
+++ src/components/BookLine.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 import {paginate} from '../util';
-import {ChunkedLine} from './lines';
+import {WeightedLine, BasicLine, NumberLine, ChunkedLine} from './lines';
 import './BookLine.css';
 
 /**
@@ -16,7 +16,8 @@ class BookLine extends Component {
     super(props);
 
     this.state = {
-      occurences: this.findOccurences(props.text, props.character)
+      occurences: this.findOccurences(props.text, props.character),
+      type: 'number'
     };
   } 
 
@@ -24,9 +25,49 @@ class BookLine extends Component {
     let occ = this.state.occurences;
     let curr = this.props.current;
 
+    let line;
+    switch (this.state.type) {
+      case 'basic':
+        line = <BasicLine points={occ} current={curr} />;
+        break;
+      case 'weighted':
+        line = <WeightedLine points={occ} current={curr} />;
+        break;
+      case 'number':
+        line = <NumberLine points={occ} current={curr} />;
+        break;
+      case 'chunked':
+        line = <ChunkedLine points={occ} current={curr} chunk={3} />;
+        break;
+      default:
+        throw 'invalid line type';
+    }
+
     return (
       <div className="BookLine">
-        <ChunkedLine points={occ} current={curr} chunk={3} />
+        <div>
+          <button 
+            onClick={()=>this.setState({
+              ...this.state,
+              type: 'weighted'
+            })} >Weighted </button>
+          <button 
+            onClick={()=>this.setState({
+              ...this.state,
+              type: 'basic'
+            })} >Basic</button>
+          <button 
+            onClick={()=>this.setState({
+              ...this.state,
+              type: 'number'
+            })} >Number</button>
+          <button 
+            onClick={()=>this.setState({
+              ...this.state,
+              type: 'chunked'
+            })} >Chunked</button>
+        </div>
+        {line}
       </div>
     );
   }
@@ -42,6 +83,8 @@ class BookLine extends Component {
         }
       });
   }
+
+  
 }
 
 BookLine.defaultProps = {
