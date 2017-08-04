d2b27a6a1916399c07e1708a1eb1a7ea48672d15
Stuart Thomson <swdt1@students.waikato.ac.nz>
2017-08-04 13:20:33 +1200

    Remove old Loading component

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,9 @@
 import Page from './Page';
-import Loading from './Loading';
 import Paragraph from './Paragraph';
 import Reader from './Reader';
 
 export {
   Page,
-  Loading,
   Paragraph,
   Reader
 };
